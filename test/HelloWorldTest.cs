9cb674d820bfc3f194bdd13d85e61e3cc9f08b56
anagri <amir.nagri@gmail.com>
2009-09-26 17:45:33 +0530

    adding dummy test for build

---
+++ test/HelloWorldTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace sharekhan
+{
+    [TestFixture]
+    public class HelloWorldTest
+    {
+        [Test]
+        public void ShouldSayHello()
+        {
+            Assert.AreEqual("Hello", new HelloWorld().SayHello());
+        }
+    }
+}
