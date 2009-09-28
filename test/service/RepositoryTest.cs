a93eb99049ff8399ba8c0c76693b866c198efce3
VasanthAmana <vasanthaman@gmail.com>
2009-09-28 15:55:46 +0530

    HBM for Instrument

---
+++ test/service/RepositoryTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Sharekhan.domain;
+
+namespace Sharekhan.service
+{
+    [TestFixture]
+    public class RepositoryTest
+    {
+        [Test]
+        public void TestSave()
+        {
+            Instrument instrument = new MutualFund();
+            
+
+        }
+    }
+}
