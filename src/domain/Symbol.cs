f046e117b1c3659dd0c479b88f4579f45c6c5bd8
VasanthAmana <vasanthaman@gmail.com>
2009-09-28 17:17:37 +0530

    fixing build

---
+++ src/domain/Symbol.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sharekhan.domain
+{
+    public class Symbol
+    {
+        private String _value;
+
+        protected virtual String Value { get; set; }
+
+        private Symbol()
+        {
+        }
+
+        public Symbol(String value)
+        {
+            this._value = value;
+        }
+
+    }
+}
