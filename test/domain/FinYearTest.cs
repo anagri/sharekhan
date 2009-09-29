7fade62a77ae37adfaf4fc7b2b72044d0bf0f1e1
Sriki <sriki77@gmail.com>
2009-09-29 17:11:49 +0530

    Add test to check for negative fin years

--- test/domain/FinYearTest.cs
+++ test/domain/FinYearTest.cs
@@ -19,5 +19,20 @@ namespace ShareKhan.domain
                 Console.WriteLine(e);
             }
         }
+
+        [Test]
+        [Ignore]
+        public void FinYearInputShouldNotBeNegative()
+        {
+            try
+            {
+                FinYear finYear = new FinYear(-2009, -2011);
+                Assert.Fail("Should Throw an exception");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
     }
 }
