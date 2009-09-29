68a1b9fcd95ad6d55fdc66eced6f68ec9f705187
Srikanth Nutigattu <Srikanth_Nutigattu@thoughtworks.com>
2009-09-29 16:09:47 +0530

    added new files

---
+++ src/domain/BuyTransaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sharekhan.domain;
+
+namespace ShareKhan.domain
+{
+    public class BuyTransaction:Transaction
+    {
+        public virtual double Tax { get; set; }
+        public virtual double Brokerage { get; set; }
+
+        public BuyTransaction(){}
+
+
+        public BuyTransaction(DateTime date, Instrument instrument, int quantity, double amount, double tax, double brokerage)
+            : base(date, instrument, quantity, amount)
+        {
+            this.Tax = tax;
+            this.Brokerage = brokerage;
+        }
+
+
+        public override Price TransactionAmount()
+        {
+
+            return new Price(Amount+Tax+Brokerage);
+        }
+    }
+}
---
+++ src/domain/SellTransaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sharekhan.domain;
+
+namespace ShareKhan.domain
+{
+    class SellTransaction:Transaction
+    {
+        public virtual double Tax { get; set; }
+        public virtual double Brokerage { get; set; }
+
+        public SellTransaction(){}
+
+
+        public SellTransaction(DateTime date, Instrument instrument, int quantity, double amount, double tax, double brokerage)
+            : base(date, instrument, quantity, amount)
+        {
+            this.Tax = tax;
+            this.Brokerage = brokerage;
+        }
+
+
+        public override Price TransactionAmount()
+        {
+            return new Price(Amount+Tax+Brokerage);
+        }
+    }
+}
