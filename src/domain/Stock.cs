6ccbf6dff1188dd149917b1e3746caf20d3cf27c
Sriki <sriki77@gmail.com>
2009-09-29 17:06:04 +0530

    Added default constructor

--- src/domain/Stock.cs
+++ src/domain/Stock.cs
@@ -2,6 +2,10 @@
 {
     public class Stock : Instrument
     {
+        public Stock()
+        {
+        }
+
         public Stock(Symbol symbol, Price currentPrice, string description)
             : base(symbol, currentPrice, description)
         {
