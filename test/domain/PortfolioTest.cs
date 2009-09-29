030114fb4d506ab06fa6eb03ef9a20c373bb26c2
Sriki <sriki77@gmail.com>
2009-09-29 15:43:24 +0530

    Cleaned up portfolio class

--- test/domain/PortfolioTest.cs
+++ test/domain/PortfolioTest.cs
@@ -11,15 +11,6 @@ namespace ShareKhan.domain
     [TestFixture]
     public class PorfolioTest
     {
-        [Test]
-        public void should_get_invested_value()
-        {
-            Portfolio portfolio = new Portfolio();
-            portfolio.addTransaction(new Transaction("Trans001", 10,
-                                                     new MutualFund(new Symbol("RIL"), new Price(1000),"Reliance MF"),
-                                                     DateTime.Today, 100, 100));
-
-
-        }
+       
     }
 }
