fa37a0ba6d3ed67ec5a71c5a045fd78b59688e4f
Sriki <sriki77@gmail.com>
2009-09-29 15:57:35 +0530

    re-org of persist layer

---
+++ src/persist/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShareKhan.service
+{
+    public interface IRepository
+    {
+ 
+        void Save(Object entity);
+
+        T Lookup<T>(int id);
+
+        void Delete(Object entity);
+
+        void Attach(Object entity);
+
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollbackTransaction();
+    }
+}
---
+++ test/persist/RepositoryTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+using Sharekhan.domain;
+using ShareKhan.service;
+
+namespace Sharekhan.service
+{
+    [TestFixture]
+    class RepositoryTest
+    {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            repository.BeginTransaction();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            repository.RollbackTransaction();
+        }
+
+        #endregion
+
+        private IRepository repository;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            repository = new Repository();
+        }
+
+
+        [Test]
+        public void TestLookupAfterSave()
+        {
+            var hundredRuppees = new Price(100);
+            var relianceMutuals = new Symbol("RELTICK");
+            Instrument instrument = new MutualFund(relianceMutuals, hundredRuppees, "Test Fund");
+
+
+            repository.Save(instrument);
+
+            var actual = repository.Lookup<Instrument>(Convert.ToInt16(instrument.Id));
+
+            Assert.AreEqual(typeof (MutualFund), actual.GetType());
+            Assert.AreEqual(instrument, actual);
+        }
+
+        [Test]
+        public void TestSave()
+        {
+            var hundredRuppees = new Price(100);
+            var relianceMutuals = new Symbol("RELTICK");
+            Instrument instrument = new MutualFund(relianceMutuals, hundredRuppees, "Test Fund");
+
+            Assert.AreEqual(0, instrument.Id);
+            repository.Save(instrument);
+            Assert.AreNotEqual(0, instrument.Id);
+            Assert.IsNotNull(instrument.Id);
+        }
+    }
+}
