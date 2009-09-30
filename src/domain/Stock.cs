95b4e17d83efc47198628962fa1c8ab71295868a
VasanthAmana <vasanthaman@gmail.com>
2009-09-30 17:08:54 +0530

    wip story 6

--- src/domain/Stock.cs
+++ src/domain/Stock.cs
@@ -1,4 +1,6 @@
-﻿namespace Sharekhan.domain
+﻿using System;
+
+namespace Sharekhan.domain
 {
     public class Stock : Instrument
     {
@@ -9,6 +11,11 @@
         public Stock(Symbol symbol, Price currentPrice, string description)
             : base(symbol, currentPrice, description)
         {
+        }
+
+        public virtual CashDividendTransaction CreateDividendTransaction(Price price,DateTime transactionDate)
+        {
+            return new CashDividendTransaction(this, price, transactionDate);
         }
     }
 }
