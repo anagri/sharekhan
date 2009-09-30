e19726bddba24676274a466340de295b63b7ef72
Sriki <sriki77@gmail.com>
2009-09-30 15:00:34 +0530

    Added Curent fin year test

--- test/domain/FinYearTest.cs
+++ test/domain/FinYearTest.cs
@@ -6,26 +6,33 @@ namespace ShareKhan.domain
     [TestFixture]
     public class FinYearTest
     {
-//        [Test]
-//        public void DifferenceBetweenTheStartAndEndYearShouldBeOne()
-//        {
-//            try
-//            {
-//                var finYear = new FinYear(2009, 2011);
-//                Assert.Fail("Should Throw an exception");
-//            }
-//            catch (ArgumentException e)
-//            {
-//            }
-//        }
+        [TestCase(2009, 2011)]
+        [TestCase(2009, 2008)]
+        [ExpectedException(typeof (ArgumentException))]
+        public void EndYearShouldBeOneGreaterThanStartYear(int startYear, int endYear)
+        {
+            new FinYear(startYear, endYear);
+        }
 
         [TestCase(-2009, -2010)]
         [TestCase(2009, -2010)]
         [TestCase(-2009, 2010)]
-        [ExpectedException(typeof(ArgumentException))]
+        [ExpectedException(typeof (ArgumentException))]
         public void FinYearInputShouldBeNonZeroPositive(int startYear, int endYear)
         {
             new FinYear(startYear, endYear);
+        }
+
+        [Ignore]
+        [Test]
+        [TestCase(2009, 2010, Result = true)]
+        [TestCase(2008, 2009, Result = false)]
+        [TestCase(2010, 2011, Result = false)]
+        [TestCase(2011, 2012, Result = false)]
+        public bool ShouldAbleToDetermineCurFinYear(int startYear, int endYear)
+        {
+            var year = new FinYear(startYear, endYear);
+            return year.IsCurrent();
         }
     }
 }
