24af9aab017f78e117f4dd4060fb6dd6ae8d1754
Sriki <sriki77@gmail.com>
2009-09-30 16:46:03 +0530

    Added test to get taxable days

--- test/domain/FinYearTest.cs
+++ test/domain/FinYearTest.cs
@@ -40,9 +40,22 @@ namespace ShareKhan.domain
         [Test]
         public void ShouldGetTheTaxableDayForFinYear()
         {
-            Assert.Equals(DateTime.Today, new FinYear(2009, 2010).GetLastTaxableDay());
-            Assert.Equals(new DateTime(2009,3,31)/*31st March 2009*/, new FinYear(2008, 2009).GetLastTaxableDay());
-            Assert.Equals(new DateTime(2010,4,1)/*1st April 2010*/, new FinYear(2010, 2011).GetLastTaxableDay());
+            var apr1St2008 = new DateTime(2008, 4, 1);
+            
+            var apr1St2009 = new DateTime(2009, 4, 1);
+            var mar31St2009 = new DateTime(2009, 3, 31);
+
+            var apr1St2010 = new DateTime(2010, 4, 1);
+            
+
+            var taxableDaysOf2009 = new FinYear.TaxableDateRange(apr1St2009, DateTime.Now);
+            Assert.Equals(taxableDaysOf2009,new FinYear(2009, 2010).GetTaxableDays());
+
+            var taxableDaysOf2008 = new FinYear.TaxableDateRange(apr1St2008, mar31St2009);
+            Assert.Equals(taxableDaysOf2008, new FinYear(2008, 2009).GetTaxableDays());
+
+            var taxableDaysOf2010 = new FinYear.TaxableDateRange(apr1St2010, apr1St2010);
+            Assert.Equals(taxableDaysOf2010, new FinYear(2010, 2011).GetTaxableDays());
         }
     }
 }
