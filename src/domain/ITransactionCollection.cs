d84e9adf9e1da5b02b5aba094088f4a88d930f3b
manishkris <manishkris@gmail.com>
2009-09-30 17:16:43 +0530

    Made some tests work correctly

--- src/domain/ITransactionCollection.cs
+++ src/domain/ITransactionCollection.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Sharekhan.domain
 {
-    public interface ITransactionCollection
+    public interface ITransactionCollection : IEnumerator<Transaction>
     {
-        
+        bool Add(Transaction transaction);
+        bool Remove(int Id);
     }
 }
