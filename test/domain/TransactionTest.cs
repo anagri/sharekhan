c420b49a4a854d73e8a4e20ae44d0546a97b975b
vijayaraj210 <vijayaraj210@yahoo.com>
2009-10-01 14:35:56 +0530

    MF Tests

--- test/domain/TransactionTest.cs
+++ test/domain/TransactionTest.cs
@@ -32,6 +32,41 @@ namespace Sharekhan.domain
             Assert.IsNotNull(transaction);
             Assert.AreEqual(transaction.Id, sellTransaction.Id);
         }
+        [Test]
+        public void Verify_Purchase_MutualFund()
+        {
+            MutualFundParams parameters = new MutualFundParams();
+            parameters.FundHouse = "SBI";
+            parameters.FundNm = "Magnum";
+            parameters.DivOption = DivOption.Growth.ToString();
+
+            Instrument mf = new MutualFund(new Symbol("SBI"), new Price(10.00), "SBI Mutual Fund", parameters);
+
+            Transaction purchaseTransaction = new BuyTransaction(DateTime.Today, mf, 5, new Price(10.00), 5.00, 3.00);
+            repository.Save(purchaseTransaction);
+            Assert.IsTrue(purchaseTransaction.Id > 0);
+            Console.WriteLine(purchaseTransaction.Id);
+            Transaction transaction = repository.Lookup<Transaction>(purchaseTransaction.Id);
+            Assert.IsNotNull(transaction);
+            Console.WriteLine(transaction.Id);
+            Assert.AreEqual(transaction.Id, purchaseTransaction.Id);
+        }
+
+        [Test]
+        public void Verify_Sell_MutualFund()
+        {
+            MutualFundParams parameters = new MutualFundParams();
+            parameters.FundHouse = "SBI";
+            parameters.FundNm = "Magnum";
+            parameters.DivOption = DivOption.Growth.ToString();
+            Instrument mf = new MutualFund(new Symbol("SBI"), new Price(10.00), "SBI Mutual Fund", parameters);
+            Transaction sellTransaction = new SellTransaction(DateTime.Today, mf, 2, new Price(50.00), 5.00, 3.00);
+            repository.Save(sellTransaction);
+            Assert.IsTrue(sellTransaction.Id > 0);
+            Transaction transaction = repository.Lookup<Transaction>(sellTransaction.Id);
+            Assert.IsNotNull(transaction);
+            Assert.AreEqual(transaction.Id, sellTransaction.Id);
+        }
     }
 
   
