744d7b092d2f47eff2501c12124f56c7e8e979a4
unknown <prakashk@.corporate.thoughtworks.com>
2009-11-03 17:46:40 +0530

    Test case for getting transaction of instrument added for Short term tax calculation story

--- test/domain/TransactionTest.cs
+++ test/domain/TransactionTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Moq;
 using NUnit.Framework;
+using ShareKhan.persist;
 using Sharekhan.service;
 using ShareKhan.domain;
 
@@ -92,6 +94,26 @@ namespace Sharekhan.domain
 
             Assert.AreEqual(2, longTerm);
             Assert.AreEqual(2, shortTerm);
+        }
+
+        [Test]
+        public void ShouldBeAbleToGetTransactionsForInstrument()
+        {
+            Instrument share = new Stock(new Symbol("REL"), new Price(10.00), "Reliance Power");
+            Transaction buyTransaction1 = new BuyTransaction(new DateTime(2008, 06, 01), share, 10, new Price(10.00), 5.00, 3.00);
+            Transaction sellTransaction1 = new SellTransaction(new DateTime(2008, 12, 01), share, 2, new Price(20.00), 5.00, 3.00);
+            Transaction buyTransaction2 = new BuyTransaction(new DateTime(2009, 06, 01), share, 12, new Price(50.00), 5.00, 3.00);
+            Transaction sellTransaction2 = new SellTransaction(new DateTime(2009, 12, 01), share, 20, new Price(100.00), 5.00, 3.00);
+
+            List<Transaction> listTransaction = new List<Transaction> { buyTransaction1, sellTransaction1, buyTransaction2, sellTransaction2 };
+            var mock = new Mock<IRepository>();
+            mock.Setup(repo => repo.ListTransactionsByInstrumentId<Transaction>(share.Id)).Returns(listTransaction);
+
+
+            IList<Transaction> list = mock.Object.ListTransactionsByInstrumentId<Transaction>(share.Id);
+
+
+            Assert.AreEqual(4, list.Count);
         }
     }
 }
