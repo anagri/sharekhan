4c3b2e9f1ddc1f47d09dcd64a78772ee8704ac38
unknown <arunpr@.corporate.thoughtworks.com>
2009-11-05 16:34:43 +0530

    Test cases for TaxCalculation

--- test/domain/ShortTermTaxCalculatorTest.cs
+++ test/domain/ShortTermTaxCalculatorTest.cs
@@ -71,6 +71,110 @@ namespace Sharekhan.test.domain
             Assert.AreEqual(sellTransaction2, sellStack.Pop());
 
         }
+        [Test]
+        public void ShouldBeAbleToCalculateTaxGivenABuyStackAndSellStackWithOneValueEachAndTheyAreEqual()
+        {
+            Instrument share = new Stock(new Symbol("REL"), new Price(10.00), "Reliance Power");
+            Transaction buyTransaction1 = new BuyTransaction(new DateTime(2008, 06, 01), share, 10, new Price(10.00), 5.00, 3.00);
+            Transaction sellTransaction1 = new SellTransaction(new DateTime(2008, 12, 01), share, 10, new Price(20.00), 5.00, 3.00);
+            
+            List<Transaction> listTransaction = new List<Transaction> { buyTransaction1, sellTransaction1};
+
+            ShortTermTaxCalculator TaxCalculator = new ShortTermTaxCalculator(listTransaction);
+
+            Stack buyStack = TaxCalculator.CreateBuyTransactionStack();
+            Stack sellStack = TaxCalculator.CreateSellTransactionStack();
+
+            Assert.AreEqual(1,buyStack.Count);
+            Assert.AreEqual(1, sellStack.Count);
+
+            Price Tax;
+            Tax = TaxCalculator.CalculateTaxOverTheBuyAndSellStacks(buyStack, sellStack);
+
+            Assert.AreEqual(20, Tax.Value);
+        }
+
+        [Test]
+        public void ShouldBeAbleToCalculateTaxGivenABuyStackAndSellStackWithOneValueEachAndBuyIsMore()
+        {
+            Instrument share = new Stock(new Symbol("REL"), new Price(10.00), "Reliance Power");
+            Transaction buyTransaction1 = new BuyTransaction(new DateTime(2008, 06, 01), share, 15, new Price(10.00), 5.00, 3.00);
+            Transaction sellTransaction1 = new SellTransaction(new DateTime(2008, 12, 01), share, 10, new Price(20.00), 5.00, 3.00);
+
+            List<Transaction> listTransaction = new List<Transaction> { buyTransaction1, sellTransaction1 };
+
+            ShortTermTaxCalculator TaxCalculator = new ShortTermTaxCalculator(listTransaction);
+
+            Stack buyStack = TaxCalculator.CreateBuyTransactionStack();
+            Stack sellStack = TaxCalculator.CreateSellTransactionStack();
+
+            Assert.AreEqual(1, buyStack.Count);
+            Assert.AreEqual(1, sellStack.Count);
+
+            Price Tax;
+            Tax = TaxCalculator.CalculateTaxOverTheBuyAndSellStacks(buyStack, sellStack);
+
+            Assert.AreEqual(20, Tax.Value);
+            Assert.AreEqual(1,buyStack.Count);
+            Assert.AreEqual(0, sellStack.Count);
+        }
+
+        [Test]
+        public void ShouldBeAbleToCalculateTaxGivenABuyStackAndSellStackWithOneValueEachAndSellIsMore()
+        {
+            Instrument share = new Stock(new Symbol("REL"), new Price(10.00), "Reliance Power");
+            Transaction buyTransaction1 = new BuyTransaction(new DateTime(2008, 06, 01), share, 10, new Price(10.00), 5.00, 3.00);
+            Transaction sellTransaction1 = new SellTransaction(new DateTime(2008, 12, 01), share, 15, new Price(20.00), 5.00, 3.00);
+
+            List<Transaction> listTransaction = new List<Transaction> { buyTransaction1, sellTransaction1 };
+
+            ShortTermTaxCalculator TaxCalculator = new ShortTermTaxCalculator(listTransaction);
+
+            Stack buyStack = TaxCalculator.CreateBuyTransactionStack();
+            Stack sellStack = TaxCalculator.CreateSellTransactionStack();
+
+            Assert.AreEqual(1, buyStack.Count);
+            Assert.AreEqual(1, sellStack.Count);
+
+            Price Tax;
+            Tax = TaxCalculator.CalculateTaxOverTheBuyAndSellStacks(buyStack, sellStack);
+
+            Assert.AreEqual(20, Tax.Value);
+            Assert.AreEqual(0, buyStack.Count);
+            Assert.AreEqual(1, sellStack.Count);
+        }
+
+        [Test]
+        public void ShouldBeAbleToCalculateTaxGivenABuyStackAndSellStackWithMultipleValues()
+        {
+            Instrument share = new Stock(new Symbol("REL"), new Price(10.00), "Reliance Power");
+            Transaction buyTransaction1 = new BuyTransaction(new DateTime(2008, 06, 01), share, 20, new Price(10.00), 5.00, 3.00);
+            Transaction sellTransaction1 = new SellTransaction(new DateTime(2009, 01, 01), share, 10, new Price(20.00), 5.00, 3.00);
+            Transaction buyTransaction2 = new BuyTransaction(new DateTime(2008, 12, 01), share, 10, new Price(10.00), 5.00, 3.00);
+            Transaction sellTransaction2 = new SellTransaction(new DateTime(2009, 08, 01), share, 10, new Price(20.00), 5.00, 3.00);
+
+
+            List<Transaction> listTransaction = new List<Transaction> { buyTransaction1, sellTransaction1, buyTransaction2, sellTransaction2 };
+
+            ShortTermTaxCalculator TaxCalculator = new ShortTermTaxCalculator(listTransaction);
+
+            Stack buyStack = TaxCalculator.CreateBuyTransactionStack();
+            Stack sellStack = TaxCalculator.CreateSellTransactionStack();
+
+            Assert.AreEqual(2, buyStack.Count);
+            Assert.AreEqual(2, sellStack.Count);
+
+            Price Tax;
+            Tax = TaxCalculator.CalculateTaxOverTheBuyAndSellStacks(buyStack, sellStack);
+
+//            Assert.AreEqual(0, buyStack.Count);
+//            Assert.AreEqual(0, sellStack.Count);
+
+            Assert.AreEqual(20, Tax.Value);
+            
+        }
+
+
 
         
 
